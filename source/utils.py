04dcef38be14cc900af9410a4d1aeacee3e6c041
marc-antoineallard74 <allard.marcantoine@outlook.fr>
2024-06-10 15:54:51 +0200

    utils plot

--- source/utils.py
+++ source/utils.py
@@ -1,7 +1,22 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def annualized_metrics(returns):
     mean_return = returns.mean() * 12
     std_dev = returns.std() * np.sqrt(12)
     sharpe_ratio = mean_return / std_dev
     return mean_return, std_dev, sharpe_ratio
+
+def  plot_strategy_performance(data, metrics, columns):
+    fig, axes = plt.subplots(1, len(metrics), figsize=(18, 6))
+
+
+    for i, metric in enumerate(metrics):
+        ax = axes[i]
+        data.plot(kind='bar', x='Metric', y=[columns],
+                    ax=ax, legend=True)
+        ax.set_title(metric)
+        ax.set_ylabel(metric)
+
+    plt.tight_layout()
+    plt.show()
