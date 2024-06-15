2f78df9ce1e26463489cb1a28fa7830744de9114
paultltc <paul.teiletche@epfl.ch>
2024-06-15 20:15:46 +0200

    Updating STRAT

--- source/utils.py
+++ source/utils.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_theme(style='whitegrid')
 
-def annualized_metrics(returns):
-    mean_return = returns.mean() * 12
+def annualized_metrics(returns, rf):
+    mean_return = (returns-rf).mean() * 12
     std_dev = returns.std() * np.sqrt(12)
     sharpe_ratio = mean_return / std_dev
     return mean_return, std_dev, sharpe_ratio
